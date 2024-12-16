7373d954df3e3ebcd03b941e2e818584717d50e3
LindeborgLove <lindeborglove@gmail.com>
2024-12-16 15:14:56 +0100

    Added test

--- test/DataCollector/dataCollector.test.cpp
+++ test/DataCollector/dataCollector.test.cpp
@@ -8,6 +8,11 @@ TEST_CASE("Testing DataCollector class", "[DataCollector],[Class],[Data]") {
     CHECK(dataCollector.getSensorData(1)->size() == 1);
     
     dataCollector.addData(1, {});
-    CHECK(dataCollector.getSensorData(1)->size() == 2);
+    CHECK(dataCollector.getSensorData().at(1).size() == 2);
 
+    CHECK_FALSE(dataCollector.getSensorData().at(1).empty());
+
+    // Currently unfinished, will be tested later.
+    // dataCollector.setDataReady(true);
+    // CHECK_FALSE(dataCollector.getSensorData(1)->size());
 }
