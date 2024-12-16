591305776e1b82d423e040d41866652eb80c6f92
LindeborgLove <lindeborglove@gmail.com>
2024-12-16 15:24:45 +0100

    Added comments

--- src/dataCollector.cpp
+++ src/dataCollector.cpp
@@ -5,21 +5,27 @@ DataCollector::DataCollector()
 
 }
 
+/** Tells the class that the data is not ready to be summarized */
 void DataCollector::resetFlag()
 {
     this->dataReady = false;
 }
 
+/** If true the class values will be emptied later. */
 void DataCollector::setDataReady(bool ready)
 {
     this->dataReady = ready;
 }
 
+/** Inserts sensor data into a sensor using the sensors id. */
 void DataCollector::addData(int sensorId, SensorData data)
 {
     this->m_sensorData[sensorId].push(data);
 }
 
+/**
+ * @returns a pointer to data from a specified sensor or `nullptr` if no data exists. 
+ */
 const std::queue<SensorData> *DataCollector::getSensorData(int sensorId) const noexcept
 {
     if (this->m_sensorData.contains(sensorId))
@@ -28,6 +34,9 @@ const std::queue<SensorData> *DataCollector::getSensorData(int sensorId) const n
         return nullptr;
 }
 
+/**
+ * @returns a map with all sensors data that is currently stored in the class.
+ */
 const std::map<int, std::queue<SensorData>> DataCollector::getSensorData() const noexcept
 {
     return this->m_sensorData;
